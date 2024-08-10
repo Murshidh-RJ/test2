916272d2ae2e59504550eb71def2bed670387371
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-08-10 07:16:04 +0530

    betting update

--- project.js
+++ project.js
@@ -13,4 +13,19 @@ const deposit = () => {
   }
 };
 
-deposit();
+const getNumberOfLines = () => {
+  while (true){
+    const lines = prompt("Enter the number of lines you want to bat on (1 - 3) : ");
+    const numberOfLines = parseFloat(lines);
+ 
+     if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >3){
+     console.log("invalid number of lines,try again.")
+     }else{
+       return numberOfLines;
+     }
+   }
+ };
+
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
